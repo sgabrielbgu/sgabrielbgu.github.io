813704f95538e89081aa0ee220cb32776150c79e
sgabrielbgu <134926810+sgabrielbgu@users.noreply.github.com>
2026-06-23 12:31:31 +0000

    Deploying to gh-pages from @ sgabrielbgu/sgabrielbgu.github.io@19d1f1bd209d5dd214e70acc70870143d5f97e00 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "sgabrielbgu/sgabrielbgu.github.io",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "",
     "data-category-id": "",
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -54,32 +54,7 @@ ninja.data = [{
         },{id: "news-funded-m-sc-ph-d-postdoc-positions-available-research-positions-are-available-in-areas-related-to-router-architectures-advanced-networking-and-software-defined-networks-additional-positions-are-available-in-areas-related-to-cloud-security-those-interested-are-invited-to-apply-by-email-making-sure-to-include-a-detailed-cv-and-academic-transcripts-ph-d-postdoc-candidates-should-also-include-a-copy-of-their-m-sc-ph-d-thesis-cse-cs-ee-graduates-are-encouraged-to-apply-although-strong-candidates-with-a-different-background-would-also-be-considered-requirements-include-strong-interest-in-research-fast-learning-abilities-as-well-as-strong-analytical-skills-and-programming-skills",
           title: 'Funded M.Sc./Ph.D./PostDoc positions available. Research positions are available in areas related to router...',
           description: "",
-          section: "News",},{id: "projects-networking-for-ai",
-          title: 'Networking for AI',
-          description: "In-network computing and network-level optimization of parallelism strategies for distributed machine learning.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project.html";
-            },},{id: "projects-caching",
-          title: 'Caching',
-          description: "Algorithms and competitive analysis for network caching under limited or imprecise knowledge.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project.html";
-            },},{id: "projects-edge-cloud-resource-allocation",
-          title: 'Edge-Cloud Resource Allocation',
-          description: "Service provisioning, placement, and embedding with provable guarantees in cloud and edge-cloud environments.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project.html";
-            },},{id: "projects-cloud-amp-microarchitectural-security",
-          title: 'Cloud &amp;amp; Microarchitectural Security',
-          description: "Security implications of co-location and resource sharing in virtualized and cloud environments.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project.html";
-            },},{id: "projects-buffer-management-amp-scheduling",
-          title: 'Buffer Management &amp;amp; Scheduling',
-          description: "Competitive analysis of buffer management and scheduling algorithms under limited knowledge and resource constraints.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/buffer_management.html";
-            },},{
+          section: "News",},{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
